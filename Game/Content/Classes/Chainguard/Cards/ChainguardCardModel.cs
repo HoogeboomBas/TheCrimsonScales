f4574619be4cf42766ab8642decf3f37ae1ed468
Sergey Burgsdorf <sergey.burgsdorf@gmail.com>
2025-09-07 22:49:14 +0200

    Change Png to Jpg for Cards

--- Game/Content/Classes/Chainguard/Cards/ChainguardCardModel.cs
+++ Game/Content/Classes/Chainguard/Cards/ChainguardCardModel.cs
@@ -2,7 +2,7 @@
 	where TTop : ChainguardCardSide, new()
 	where TBottom : ChainguardCardSide, new()
 {
-	protected override string TexturePath => "res://Content/Classes/Chainguard/Cards.png";
+	protected override string TexturePath => "res://Content/Classes/Chainguard/Cards.jpg";
 	protected override int ColumnCount => 6;
 	protected override int RowCount => 5;
 }
