d41f44f22d186d3e7d1c62c34a18687de32d7841
Sergey Burgsdorf <sergey.burgsdorf@gmail.com>
2025-09-08 12:39:58 +0200

    Implement Shackle

--- Game/Content/Classes/Chainguard/Chainguard.cs
+++ Game/Content/Classes/Chainguard/Chainguard.cs
@@ -1,4 +1,22 @@
+using Fractural.Tasks;
+
 public partial class Chainguard : Character
 {
-	private ChainguardModel _chainguardModel;
+	public override async GDTask OnScenarioSetupCompleted()
+	{
+		await base.OnScenarioSetupCompleted();
+
+		object subscriber = new object();
+
+		ScenarioEvents.InflictConditionEvent.Subscribe(this, subscriber,
+			canApply: parameters => parameters.Condition is Shackle && parameters.PotentialAbilityState != null,
+			apply: async parameters =>
+			{
+				Shackle shackle = (Shackle)parameters.Condition;
+				shackle.AddCause(parameters.PotentialAbilityState.Performer);
+				await GDTask.CompletedTask;
+			},
+			EffectType.MandatoryBeforeOptionals
+		);
+	}
 }
