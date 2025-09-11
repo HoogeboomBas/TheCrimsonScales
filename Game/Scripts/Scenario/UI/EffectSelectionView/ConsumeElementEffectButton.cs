d2d9419f0496f8b3709839927e825aa8f0e50905
Vanja Aradski <vanjaaradski@gmail.com>
2025-09-11 22:38:58 +0200

    Consume multiple elements

--- Game/Scripts/Scenario/UI/EffectSelectionView/ConsumeElementEffectButton.cs
+++ Game/Scripts/Scenario/UI/EffectSelectionView/ConsumeElementEffectButton.cs
@@ -1,26 +1,53 @@
 ﻿using Godot;
+using System.Collections.Generic;
 
 public partial class ConsumeElementEffectButton : EffectButton<ConsumeElementEffectButton.Parameters>
 {
 	public class Parameters : EffectButtonParameters
 	{
-		public override string ScenePath => "res://Scenes/Scenario/UI/EffectButtons/ConsumeElementEffectButton.tscn";
+		public override string ScenePath => "res://Scenes/Scenario/UI/EffectButtons/ConsumeElementsEffectButton.tscn";
 
-		public Element Element { get; }
+		public IReadOnlyList<Element> Elements { get; }
 
+		public Parameters(IEnumerable<Element> elements)
+		{
+			Elements = new List<Element>(elements);
+		}
+
+		// For backward compatibility, allow single-element constructor
 		public Parameters(Element element)
 		{
-			Element = element;
+			Elements = new List<Element> { element };
 		}
 	}
 
 	[Export]
-	private TextureRect _textureRect;
+	private NodePath _elementsContainerPath = "BetterButton/Panel/MarginContainer/ElementsContainer";
+
+	private HBoxContainer _elementsContainer;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		_elementsContainer = GetNode<HBoxContainer>(_elementsContainerPath);
+	}
 
 	protected override void Init(Parameters parameters)
 	{
 		base.Init(parameters);
 
-		_textureRect.Texture = ResourceLoader.Load<Texture2D>(Icons.GetElement(parameters.Element));
+		// Clean up existing icons
+		foreach (Node child in _elementsContainer.GetChildren())
+			child.QueueFree();
+
+		foreach (Element element in parameters.Elements)
+		{
+			TextureRect textureRect = new TextureRect
+			{
+				Texture = ResourceLoader.Load<Texture2D>(Icons.GetElement(element)),
+				ExpandMode = TextureRect.ExpandModeEnum.FitWidthProportional
+			};
+			_elementsContainer.AddChild(textureRect);
+		}
 	}
 }
