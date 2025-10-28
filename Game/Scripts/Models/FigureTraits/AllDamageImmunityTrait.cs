690b3ea8c6c664c1100feae8d166b43515de30f8
Sergey Burgsdorf <sergey.burgsdorf@gmail.com>
2025-10-28 15:07:08 +0100

    Add monster stats and traits

---
+++ Game/Scripts/Models/FigureTraits/AllDamageImmunityTrait.cs
@@ -0,0 +1,26 @@
+﻿using Fractural.Tasks;
+
+public class AllDamageImmunityTrait : FigureTrait
+{
+	public override void Activate(Figure figure)
+	{
+		base.Activate(figure);
+
+		ScenarioEvents.SufferDamageEvent.Subscribe(figure, this,
+			parameters => parameters.Figure == figure && parameters.WouldSufferDamage,
+			async parameters =>
+			{
+				parameters.SetDamagePrevented();
+
+				await GDTask.CompletedTask;
+			}
+		);
+	}
+
+	public override void Deactivate(Figure figure)
+	{
+		base.Deactivate(figure);
+
+		ScenarioEvents.SufferDamageEvent.Unsubscribe(figure, this);
+	}
+}
