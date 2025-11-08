b83b61ef48619fd8474158e04d5171491a44cd2c
Sergey Burgsdorf <sergey.burgsdorf@gmail.com>
2025-11-08 22:41:10 +0100

    Fix Swing on monsters

--- Game/Scripts/Scenario/Prompts/MonsterForcedMovementPrompt.cs
+++ Game/Scripts/Scenario/Prompts/MonsterForcedMovementPrompt.cs
@@ -20,8 +20,13 @@ public class MonsterForcedMovementPrompt(
 	private readonly List<Hex> _path = new List<Hex>();
 
 	private readonly List<ForcedMovementNode> _bestNodes = new List<ForcedMovementNode>();
+	private int _bestMoveSpent = 0;
 
-	protected override bool CanConfirm => _bestNodes.Any(bestNode => bestNode.Hex == _currentNode.Hex);
+	protected override bool CanConfirm => _bestNodes.Any(bestNode => bestNode.Hex == _currentNode.Hex) || 
+		(!_bestNodes.Any() && _currentNode.MoveSpent >= _bestMoveSpent);
+	// For Swing, best nodes are empty when there are no further hexes to swing
+	// In that case we also want to prevent picking an option with less hexes moved
+		
 	protected override bool CanSkip => false;
 
 	protected override void Enable()
@@ -67,7 +72,7 @@ public class MonsterForcedMovementPrompt(
 
 		if(_bestNodes.Count == 0 || _bestNodes[0].Hex == _currentNode.Hex)
 		{
-			// Cannot push/pull further
+			// Cannot push/pull/swing further
 			Skip();
 		}
 	}
@@ -88,6 +93,11 @@ public class MonsterForcedMovementPrompt(
 			_bestNodes.Clear();
 			foreach((Hex hex, ForcedMovementNode node) in _closedList)
 			{
+				if(node.MoveSpent > _bestMoveSpent)
+				{
+					_bestMoveSpent = node.MoveSpent;
+				}
+
 				_bestNodes.Add(node);
 			}
 		}
