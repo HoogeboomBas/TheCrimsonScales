a2ca87b44a864eb86e17fa4447547220009e4e0f
Sergey Burgsdorf <sergey.burgsdorf@gmail.com>
2025-11-23 20:20:26 +0100

    New mat and basic required files

---
+++ Game/Content/Classes/Hollowpact/Hollowpact.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+using Fractural.Tasks;
+using Godot;
+
+public partial class Hollowpact : Character
+{
+	public override async GDTask OnScenarioSetupCompleted()
+	{
+		await base.OnScenarioSetupCompleted();
+	}
+}
---
+++ Game/Content/Classes/Hollowpact/HollowpactModel.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Godot;
+
+public class HollowpactModel : ClassModel
+{
+	public override string Name => "Hollowpact";
+	public override MaxHealthValues MaxHealthValues => MaxHealthValues.Medium;
+	public override int HandSize => 11;
+	public override string AssetPath => "res://Content/Classes/Hollowpact";
+	public override Color PrimaryColor => Color.FromHtml("a765a9");
+	public override Color SecondaryColor => Color.FromHtml("310f33");
+
+	public override PackedScene Scene => ResourceLoader.Load<PackedScene>($"{AssetPath}/Hollowpact.tscn");
+
+	public override IList<AbilityCardModel> AbilityCards { get; } =
+	[
+		//ModelDB.Card<>(),
+	];
+}
