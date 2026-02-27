d1509e3a8bbabcc9089c06afcc1a70512ae0fd9f
Sergey <sergey@nowatch.com>
2026-02-27 17:36:38 +0100

    Revert "Fix items slots"
    This reverts commit 5a1e55b63826ea7de65b17c109bb09184c027e56.

--- Game/Scripts/Models/Items/ItemModel.cs
+++ Game/Scripts/Models/Items/ItemModel.cs
@@ -109,12 +109,12 @@ public abstract class ItemModel : AbstractModel<ItemModel>, IActionSource
 		ItemState oldItemState = ItemState;
 		ItemState = state;
 
-		if(state is ItemState.Consumed or ItemState.Spent or ItemState.UnrecoverablyConsumed)
+		if(oldItemState == ItemState.Available)
 		{
 			Unsubscribe();
 		}
 
-		if(Owner != null && oldItemState is ItemState.Consumed or ItemState.Spent)
+		if(ItemState == ItemState.Available && Owner != null)
 		{
 			Subscribe();
 		}
@@ -230,10 +230,6 @@ public abstract class ItemModel : AbstractModel<ItemModel>, IActionSource
 						throw new ArgumentOutOfRangeException();
 				}
 			}
-		}
-		else
-		{
-			await SetItemState(ItemState.Available);
 		}
 
 		await ScenarioEvents.ItemUseEndedEvent.CreatePrompt(new ScenarioEvents.ItemUseEnded.Parameters(this, Owner));
