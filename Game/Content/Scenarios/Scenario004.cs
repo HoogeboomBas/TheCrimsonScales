af956f9f54c6d661e365baa71022c7c73de23b9c
Sergey Burgsdorf <sergey.burgsdorf@gmail.com>
2026-03-28 11:55:41 +0100

    Improve scenario 4 targeting

--- Game/Content/Scenarios/Scenario004.cs
+++ Game/Content/Scenarios/Scenario004.cs
@@ -181,9 +181,8 @@ public class Scenario004 : ScenarioModel
 			ScenarioCheckEvents.CanBeTargetedCheckEvent.Subscribe(monster, this,
 				parameters =>
 					parameters.PotentialTarget == monster &&
-					parameters.Performer is not Character &&
-					parameters.PotentialAbilityState != null &&
-					parameters.PotentialAbilityState is not HealAbility.State,
+					(parameters.Performer is not Character || 
+						(parameters.PotentialAbilityState != null && parameters.PotentialAbilityState is not HealAbility.State)),
 				parameters =>
 				{
 					parameters.SetCannotBeTargeted();
