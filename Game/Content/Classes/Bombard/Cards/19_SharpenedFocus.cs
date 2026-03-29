3418aee6672d497f175adfcf83e9a3285318e10d
Sergey Burgsdorf <sergey.burgsdorf@gmail.com>
2026-03-29 12:44:50 +0200

    Add missing set performed

--- Game/Content/Classes/Bombard/Cards/19_SharpenedFocus.cs
+++ Game/Content/Classes/Bombard/Cards/19_SharpenedFocus.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Fractural.Tasks;
 using Godot;
@@ -85,6 +85,8 @@ public class SharpenedFocus : BombardCardModel<SharpenedFocus.CardTop, Sharpened
 							parameters.AbilityState.SingleTargetSetHasAdvantage();
 							ScenarioEvents.FigureTurnEndedEvent.Unsubscribe(state, this);
 							ScenarioEvents.DuringAttackEvent.Unsubscribe(state, this);
+
+							state.SetPerformed();
 							await GDTask.CompletedTask;
 						});
 
