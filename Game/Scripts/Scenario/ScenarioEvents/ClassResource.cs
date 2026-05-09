2ba6acf50cd7e1c86256e934ddb1c6062889c7b2
Sergey Burgsdorf <sergey.burgsdorf@gmail.com>
2026-05-09 11:27:43 +0200

    Remove unused files

--- Game/Scripts/Scenario/ScenarioEvents/ClassResource.cs
+++
@@ -1,8 +0,0 @@
-public abstract class ClassResource
-{
-	//public abstract void Gain<T>(T parameters, int quantity);
-	public abstract bool CheckAvailability(Figure figure, int quantity);
-	public abstract bool TryConsume(Figure figure, int quantity);
-	public abstract string GetIcon();
-	public abstract string GetText(int quantity);
-}
