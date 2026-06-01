0eb79ab0e7d9be34f9ad100d02fd47d1d88f03ff
Sergey Burgsdorf <sergey.burgsdorf@gmail.com>
2026-06-01 17:19:22 +0200

    Minor fix for miser

--- Game/Content/BattleGoals/23_Miser.cs
+++ Game/Content/BattleGoals/23_Miser.cs
@@ -15,15 +15,15 @@ public class Miser : TheCrimsonScalesBattleGoal
 		Hex leavingHex = null;
 
 		ScenarioEvents.FigureExitingHexEvent.Subscribe(this,
-			parameters => 
+			parameters =>
 				!battleGoal.ProgressFull &&
-				parameters.Figure == character && 
+				parameters.Figure == character &&
 				parameters.Hex.Room != null,
 			async parameters =>
 			{
 				leavingHex = parameters.Hex;
 
-				await GDTask.CompletedTask; 
+				await GDTask.CompletedTask;
 			}
 		);
 
@@ -31,13 +31,14 @@ public class Miser : TheCrimsonScalesBattleGoal
 			parameters =>
 				!battleGoal.ProgressFull &&
 				parameters.Figure == character &&
+				parameters.Hex.Room != null &&
 				parameters.Hex.Room != leavingHex.Room &&
 				leavingHex.Room.Hexes.Any(hex => hex.HasHexObjectOfType<Coin>()),
 			async parameters =>
 			{
 				battleGoal.AdjustProgress(1);
 
-				await GDTask.CompletedTask; 
+				await GDTask.CompletedTask;
 			}
 		);
 
