630d45440ac3f9cdb37b91dcddb01e0f6b4aea94
Bas <bc.hoogeboom@gmail.com>
2026-07-08 01:17:39 +0200

    Started on a new killing animation.

---
+++ Game/Scripts/FracturedHexObjectEffect/FracturedHexObjectEffect.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Godot;
+using SharpVoronoiLib;
+
+public partial class FracturedHexObjectEffect : Node2D
+{
+	[Export]
+	private int _shardCount;
+	[Export]
+	private Texture2D _texture;
+	[Export]
+	private Node2D _shardsParent;
+	[Export]
+	private PackedScene _shardScene;
+
+	private static readonly Vector2 TextureSize = new Vector2(256f, 256f);
+
+	private readonly List<FracturedHexObjectEffectShard> _shards = new List<FracturedHexObjectEffectShard>();
+
+	public override void _Ready()
+	{
+		base._Ready();
+
+		//Generate();
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
+
+		if(@event is InputEventKey inputEventKey && inputEventKey.Pressed)
+		{
+			switch(inputEventKey.Keycode)
+			{
+				case Key.Space:
+					Generate();
+					break;
+			}
+		}
+	}
+
+	public void Generate()
+	{
+		VoronoiPlane plane = new VoronoiPlane(0, 0, TextureSize.X, TextureSize.Y);
+
+		// Create sites
+		List<VoronoiSite> sites = new List<VoronoiSite>();
+
+		for(int i = 0; i < _shardCount; i++)
+		{
+			sites.Add(new VoronoiSite(GD.Randf() * TextureSize.X, GD.Randf() * TextureSize.Y));
+		}
+
+		plane.SetSites(sites);
+		plane.Tessellate();
+
+		List<VoronoiEdge> edges = plane.Relax();
+
+		// Build polygons
+		foreach(VoronoiSite site in sites)
+		{
+			List<Vector2> points = GetPolygonForSite(site, edges);
+
+			if(points.Count < 3)
+			{
+				continue;
+			}
+
+			CreateShard(points);
+		}
+	}
+
+	private List<Vector2> GetPolygonForSite(VoronoiSite site, List<VoronoiEdge> edges)
+	{
+		List<Vector2> points = new List<Vector2>();
+
+		foreach(var edge in edges)
+		{
+			if(edge.Left == site || edge.Right == site)
+			{
+				points.Add(
+					new Vector2(
+						(float)edge.Start.X,
+						(float)edge.Start.Y
+					)
+				);
+
+				points.Add(
+					new Vector2(
+						(float)edge.End.X,
+						(float)edge.End.Y
+					)
+				);
+			}
+		}
+
+		// Remove duplicates
+		points = points.Distinct().ToList();
+
+		// Sort around center
+		Vector2 center = Vector2.Zero;
+
+		foreach(Vector2 p in points)
+		{
+			center += p;
+		}
+
+		center /= points.Count;
+
+		points.Sort(
+			(a, b) =>
+			{
+				float aa =
+					Mathf.Atan2(
+						a.Y - center.Y,
+						a.X - center.X
+					);
+
+				float bb =
+					Mathf.Atan2(
+						b.Y - center.Y,
+						b.X - center.X
+					);
+
+				return aa.CompareTo(bb);
+			}
+		);
+
+		return points;
+	}
+
+	private void CreateShard(List<Vector2> polygon)
+	{
+		FracturedHexObjectEffectShard shard = _shardScene.Instantiate<FracturedHexObjectEffectShard>();
+		_shardsParent.AddChild(shard);
+		shard.Init(polygon.ToArray(), _texture, TextureSize);
+		_shards.Add(shard);
+	}
+}
---
+++ Game/Scripts/FracturedHexObjectEffect/FracturedHexObjectEffectShard.cs
@@ -0,0 +1,73 @@
+﻿using Godot;
+using GTweens.Builders;
+using GTweens.Easings;
+using GTweensGodot.Extensions;
+
+public partial class FracturedHexObjectEffectShard : Node2D
+{
+	[Export]
+	private Node2D _offset;
+	[Export]
+	private Polygon2D _polygon;
+
+	private Vector2 _velocity;
+	private float _angularVelocity;
+	private float _dissolveProgress;
+
+	public void Init(Vector2[] polygon, Texture2D texture, Vector2 textureSize)
+	{
+		_polygon.SetPolygon(polygon);
+		_polygon.SetUV(polygon);
+		_polygon.SetTexture(texture);
+
+		Vector2 center = Vector2.Zero;
+
+		foreach(var p in polygon)
+		{
+			center += p;
+		}
+
+		center /= polygon.Length;
+
+		_offset.SetPosition(center);
+		_polygon.SetPosition(-center);
+
+		Vector2 direction = center - textureSize / 2;
+
+		if(direction.Length() < 1)
+		{
+			direction = Vector2.Right;
+		}
+
+		// direction = direction.Normalized();
+		direction = direction.Normalized() * Mathf.Lerp(direction.Length() * 0.01f, 1, 0.7f);
+
+		this.DelayedCall(() =>
+		{
+			_velocity = direction * Mathf.Lerp(300f, 600f, GD.Randf());
+			_angularVelocity = Mathf.Lerp(-3f, 3f, GD.Randf());
+
+			GTweenSequenceBuilder.New()
+				.AppendTime(Mathf.Lerp(0.0f, 0.15f, GD.Randf()))
+				.Append(_polygon.TweenInstanceShaderPropertyFloat("progress", 1f, Mathf.Lerp(0.2f, 0.4f, GD.Randf())))
+				.Build().Play();
+			// GTweenSequenceBuilder.New()
+			// 	.AppendTime(Mathf.Lerp(0.1f, 0.25f, GD.Randf()))
+			// 	.Append(_offset.TweenScale(0f, Mathf.Lerp(0.1f, 0.2f, GD.Randf())).SetEasing(Easing.OutQuad))
+			// 	.Build().Play();
+		}, Mathf.Lerp(0.4f, 0.5f, GD.Randf()));
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+
+		if(_velocity != Vector2.Zero)
+		{
+			_velocity += Vector2.Down * (float)delta * 1000;
+		}
+
+		SetPosition(Position += (float)delta * _velocity);
+		_offset.SetRotation(_offset.Rotation + (float)delta * _angularVelocity);
+	}
+}
