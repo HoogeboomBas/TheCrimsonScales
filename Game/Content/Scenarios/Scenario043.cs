3b694e076c491fdebe1434403ad0bf09e5a1cd98
Sergey <sergey@nowatch.com>
2026-08-04 01:16:08 +0200

    Fixes for scenario 43

--- Game/Content/Scenarios/Scenario043.cs
+++ Game/Content/Scenarios/Scenario043.cs
@@ -125,10 +125,13 @@ public class Scenario043 : ScenarioModel
 							.Where(hex => hex.IsEmpty() || (hex.IsUnoccupied() && hex.GetHexObjectOfType<Trap>() != null)));
 					}, [typeof(Boulder1HObstacle)], "Select a boulder to move");
 
-				movedToHex.GetHexObjectOfType<Trap>()?.Destroy();
-				foreach(Coin coin in movedToHex.GetHexObjectsOfType<Coin>())
+				if(movedToHex != null)
 				{
-					await coin.Destroy();
+					movedToHex.GetHexObjectOfType<Trap>()?.Destroy();
+					foreach(Coin coin in movedToHex.GetHexObjectsOfType<Coin>())
+					{
+						await coin.Destroy();
+					}
 				}
 			},
 			EffectType.Selectable,
